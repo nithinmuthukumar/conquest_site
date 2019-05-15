90a41c5755cb160722a85b2ad31539a418cb181f
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-05-15 00:20:06 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <NavigationBar v-bind:curpage="curpage"></NavigationBar>
+  </div>
+</template>
+<script>
+  import NavigationBar from "./components/NavigationBar.vue"
+
+  export default {
+    name: 'app',
+    components: {
+      NavigationBar
+
+    },
+    data(){
+      return {
+        user:"",
+        curpage:"Home"
+
+      }
+    },
+    props:{
+      switchPage:function () {
+
+      }
+
+    },
+    methods:{
+
+
+
+
+
+
+    },
+    mounted: function(){
+
+
+    }
+  }
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-nav pills fill>
+            <b-nav-item :class="{active:name=='Home'}">Home{{curpage}}</b-nav-item>
+            <b-nav-item :class="{active:name=='Login'}">Login</b-nav-item>
+            <b-nav-item :class="{active:name=='Wiki'}">Wiki</b-nav-item>
+            <b-nav-item :class="{active:name=='Play'}">Play</b-nav-item>
+        </b-nav>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "NavigationBar",
+        props:["curpage"]
+    }
+</script>
+
+<style scoped>
+
+</style>
